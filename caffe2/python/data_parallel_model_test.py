88df9d2be7af72c08715aa5408ed2fead8995273
Doug Friedman <dfriedm3@gmail.com>
2018-10-26 15:09:36 +0000

    resolve test failure if # cuda devices == 1

--- caffe2/python/data_parallel_model_test.py
+++ caffe2/python/data_parallel_model_test.py
@@ -859,7 +859,7 @@ class RecurrentNetworkParallelTest(TestCase):
         total batchsize, independent of number of GPUs/CPUs.
         '''
         for gpu in [True, False]:
-            if gpu and not workspace.has_gpu_support:
+            if gpu and (not workspace.has_gpu_support or workspace.NumCudaDevices() < 2):
                 continue
             result_2gpus = self.run_model([0, 1], gpu)
             result_1gpus = self.run_model([0], gpu)
